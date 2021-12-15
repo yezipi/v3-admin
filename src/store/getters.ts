faccb642f53a0f4c7460b4f96f0ee83694389497
zzc <974683998@qq.com>
2021-12-15 22:45:32 +0800

    调整vuex类型

--- src/store/getters.ts
+++ src/store/getters.ts
@@ -1,8 +1,10 @@
-import { State } from './state'
+import { StateConfig } from './state'
 import { GetterTree } from 'vuex'
 
-const getters: GetterTree<State, any> =  {
-
+const getters: GetterTree<StateConfig, any> =  {
+  permissions(state) {
+    return state.user ? state.user.role.permissions : []
+  }
 }
 
 export default getters
--- src/store/mutations.ts
+++ src/store/mutations.ts
@@ -1,23 +1,23 @@
 import db from '@/utils/db'
 import { MutationTree } from 'vuex'
-import { State } from './state'
+import { StateConfig } from './state'
 import Loading from '@/plugins/loading'
 
-const mutations: MutationTree<State> = {
-  updateToken(state: any, token: any) {
+const mutations: MutationTree<StateConfig> = {
+  updateToken(state, token: any) {
     state.token = token;
     db.set('token', token);
   },
-  updateUser(state: any, data: any) {
+  updateUser(state, data: any) {
     state.user = data;
     db.set('user', data);
   },
-  showLoading(state: any) {
+  showLoading(state) {
     Loading.show('加载中..')
     state.loading = Loading
     state.isLoading = true
   },
-  hideLoading(state: any) {
+  hideLoading(state) {
     if (state.isLoading) {
       state.loading.hide()
       state.isLoading = false
